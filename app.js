7e2f9fece5d1c747dae7ebea4880c4f7eee29835
bangadam <bangadam.dev@gmail.com>
2017-11-02 20:07:25 +0700

    first

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ function handleEvent(event) {
   });
 }
 
-app.listen(3000, function() {
-  console.log('server listning on port 3000');
-})
+// app.listen(3000, function() {
+//   console.log('server listning on port 3000');
+// })
